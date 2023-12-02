4d097c7a731611b34b749f9b366a46973c06cc25
Alex <110344381+axgw@users.noreply.github.com>
2023-12-02 02:45:54 +0100

    WIP: day_1_pt2

--- day1/day1_trebuchet_pt2.py
+++ day1/day1_trebuchet_pt2.py
@@ -1,13 +1,24 @@
-numbers = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+numbers = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
 
 def trebuchet(input_file):
-    final = []
+    final2 = []
+    s = []
     with open(input_file, 'r', encoding='UTF-8') as file:
-        while line := file.readline():
+        for line in file:
             final = []
+            temp = line.strip('\n')
             for word, value in numbers.items():
-                if word in line or str(value) in line:
-                    final.append(value)
-            print(final)
+                temp = temp.replace(word, value)
+            # if temp.strip().isdigit():
+            final.append(temp)
+            s = []
+            for i in range(len(final)):
+                for j in final[i]:
+                    if j.isdigit():
+                        s.append(int(j))
+            final2.append(s[0] * 10 + s[0]) if len(s) == 1 else final2.append(s[0] * 10 + s[-1])
+        print(final2)
+    total = sum(final2[k] for k in range(len(final2)))
+    print(total)
 
-trebuchet('input.txt')
+trebuchet('test.txt')
