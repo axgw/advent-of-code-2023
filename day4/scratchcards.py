6b1064723a29896ed6e259a4a9fbb7962b5972f9
Alex <110344381+axgw@users.noreply.github.com>
2023-12-05 15:10:33 +0100

    Add: day4, part 1

--- day4/scratchcards.py
+++ day4/scratchcards.py
@@ -1,7 +1,7 @@
 def scratchcard(input_file):
     total = 0
     with open(input_file, 'r', encoding='UTF=8') as file:
-        for i, line in enumerate(file):
+        for line in file:
             title, numbers = line.split(':')
             your_num, win_num = numbers.strip().split('|')
             winning_nums = [num for num in your_num.split() if num in win_num.split()]
