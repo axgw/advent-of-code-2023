19d0e870831e640c30f92075971ddc07977ad816
Alex <110344381+axgw@users.noreply.github.com>
2023-12-08 14:39:18 +0100

    Update: day3

--- day3/gear_ratios.py
+++ day3/gear_ratios.py
@@ -1,12 +1,33 @@
-# Skipping for now
-def gear_ratios(file_input):
+# lines = [
+#     "123",
+#     "4x5",
+#     "678"
+# ]
+# def get_numbers(line: str, pos: int):
+#     num = "0"
+#     for i in range(pos, len(line)):
+#         if line[i].isdigit():
+#             num += line[i]
+#         else:
+#             break
+#     return int(num)
+
+
+def gear_ratios(file_input: str):
     with open(file_input, 'r', encoding='UTF-8') as file:
-        for i, line in enumerate(file):
-            print(i, line)
-            for j, char in enumerate(line):
-                # print(j, char)
-                if char.isdigit():
-                    print(i,j,char)
+        lines = [line.rstrip() for line in file]
+        for i in range(len(lines)):
+            for j in range(len(lines)):
+                 if lines[i][j].isdigit():
+                    adjacent = [
+                        (i - 1, j - 1), (i - 1, j), (i - 1, j + 1),
+                        (i, j - 1),                     (i, j + 1),
+                        (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)
+                    ]
+                    for ai, aj in adjacent:
+                        if 0 <= ai < len(lines) and 0 <= aj < len(lines[0]):
+                            print(lines[ai][aj])
+                    print("end")
 
 
 gear_ratios('test.txt')
